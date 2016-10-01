44d933989fd4913efdc584f88739450f30df09b4
André <pumpitupnx2@hotmail.com>
2016-10-01 01:11:51 -0500

    3.- Primero Controlador

---
+++ practicas/hola_controller/controller.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Andre on 01/10/2016.
+ */
+'use strict';
+
+var app = angular.module("MyFirstApp", []);
+app.controller("MyFirstController", function ($scope) {
+    $scope.name = "André";
+});
