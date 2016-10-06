9e4892c436c3bdc3dd302341b8b593b612d870d6
André <pumpitupnx2@hotmail.com>
2016-10-06 00:24:49 -0500

    6.-Dependency Injection

---
+++ practicas/dependency_injection/controller.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Andre on 01/10/2016.
+ */
+'use strict';
+
+ angular.module("MyFirstApp", [])
+.controller("MyFirstController",["$scope", function ($scope) {
+    $scope.name = "André";
+    $scope.newComment = {};
+    $scope.comments = [
+        {
+            comment:":D",
+            username : "André"
+        },
+        {
+            comment:":C",
+            username:"Unknown"
+        }
+    ];
+
+    $scope.addComment = function () {
+        $scope.comments.push($scope.newComment);
+        $scope.newComment = {};
+
+    };
+
+}]);
