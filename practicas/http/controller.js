aa75ee876fbc242b047f5ea8d3609c44eafeb388
André <pumpitupnx2@hotmail.com>
2016-10-06 01:23:24 -0500

    8.- AJAX con http 2/2

--- practicas/http/controller.js
+++ practicas/http/controller.js
@@ -5,14 +5,32 @@
 
 angular.module("MyFirstApp", [])
     .controller("MyFirstController", function ($scope, $http) {
-        $scope.posts=[];
+        $scope.posts = [];
+        $scope.newPost = {};
         $http.get("https://jsonplaceholder.typicode.com/posts")
             .success(function (data) {
-                $scope.posts=data;
+                $scope.posts = data;
                 console.log(data);
             })
             .error(function (err) {
 
             });
+
+
+        $scope.addPost = function () {
+            $http.post("https://jsonplaceholder.typicode.com/posts", {
+                title: $scope.newPost.title,
+                body: $scope.newPost.body,
+                userId: 1
+            })
+                .success(function (data, status, header, config) {
+                    console.log(header);
+                    $scope.posts.push(data);
+                    $scope.newPost = {};
+                })
+                .error(function (err, status, header, config) {
+
+                });
+        }
 
     });
