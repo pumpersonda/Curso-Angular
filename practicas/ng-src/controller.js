9f1059089926a055fe0708d9f14d599984b4810f
André <pumpitupnx2@hotmail.com>
2016-10-24 02:31:09 -0500

    19.Crear directivas parte 2

--- practicas/ng-src/controller.js
+++ practicas/ng-src/controller.js
@@ -4,6 +4,29 @@
 'use strict';
 
 angular.module("CustomDirective", [])
+    .directive('myAutocomplete', function () {
+
+        function link(scope, element, attrs) {
+
+            $(element).autocomplete({
+                source: scope.$eval(attrs.myAutocomplete),
+                select: function (event, ui) {
+                    event.preventDefault();
+                    if (ui.item) {
+                        scope.optionSelected(ui.item.value);
+                    }
+                },
+                focus: function (event, ui) {
+                    event.preventDefault();
+                    $(this).val(ui.item.label)
+                }
+            });
+        }
+
+        return {
+            link: link
+        };
+    })
     .directive('backImg', function () {
         return function (scope, element, attrs) {
 
@@ -17,13 +40,23 @@ angular.module("CustomDirective", [])
         }
     })
     .controller("AppCtrl", ["$scope", "$http", function ($scope, $http) {
-
+        $scope.repos = [];
         $http.get("https://api.github.com/users/pumpersonda/repos")
             .success(function (data) {
-                $scope.repos = data;
+                $scope.posts = data;
+                for (var i = data.length - 1; i >= 0; i--) {
+                    var repo = data[i];
+                    $scope.repos.push(repo.name);
+                }
             })
             .error(function (err) {
                 console.log(err);
             });
 
+        $scope.optionSelected = function (value) {
+            $scope.$apply(function () {
+                $scope.main_repo = value;
+
+            })
+        }
     }]);
