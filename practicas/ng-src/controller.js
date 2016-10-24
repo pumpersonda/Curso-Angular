14abbee7ec88a11fac0483519b55194007bd6b21
André <pumpitupnx2@hotmail.com>
2016-10-24 00:22:04 -0500

    18.-Crear directivas parte 1

--- practicas/ng-src/controller.js
+++ practicas/ng-src/controller.js
@@ -4,6 +4,18 @@
 'use strict';
 
 angular.module("CustomDirective", [])
+    .directive('backImg', function () {
+        return function (scope, element, attrs) {
+
+            attrs.$observe('backImg', function (value) {
+                element.css({
+                    "background": "url(" + value + ")",
+                    "background-size": "cover",
+                    "background-position": "center"
+                });
+            });
+        }
+    })
     .controller("AppCtrl", ["$scope", "$http", function ($scope, $http) {
 
         $http.get("https://api.github.com/users/pumpersonda/repos")
