c7fea47c4740d280037c53ee9d71c7ee32059a1b
Andre Chay Sonda <pumpitupnx2@hotmail.com>
2016-12-20 01:37:58 -0600

    23.- Practica final 2/3

--- practicas/resource/js/app.js
+++ practicas/resource/js/app.js
@@ -4,16 +4,19 @@
 
 (function () {
     'use strict';
-    angular.
-    module("FinalApp",['lumx','ngRoute','ngResource'])
+    angular.module("FinalApp", ['lumx', 'ngRoute', 'ngResource'])
         .config(routeConfig);
 
     routeConfig.$inject = ['$routeProvider'];
-    function routeConfig($routeProvider){
+    function routeConfig($routeProvider) {
         $routeProvider
-            .when("/",{
-                controller:"MainController",
+            .when("/", {
+                controller: "MainController",
                 templateUrl: "templates/home.html"
+            })
+            .when("/post/:id", {
+                controller: "PostController",
+                templateUrl: "templates/post.html"
             });
     }
 
--- practicas/resource/js/controllers.js
+++ practicas/resource/js/controllers.js
@@ -7,7 +7,8 @@
  'use strict';
     angular
         .module("FinalApp")
-        .controller('MainController',MainController);
+        .controller('MainController',MainController)
+        .controller('PostController',PostController);
 
     MainController.$inject = ['$scope','$resource'];
     function MainController($scope,$resource) {
@@ -15,5 +16,19 @@
         var Users = $resource('https://jsonplaceholder.typicode.com/users/:id', {id: "@id"});
         $scope.posts = Post.query();
         $scope.users = Users.query();
+
+        $scope.removePost = function (post) {
+
+            Post.delete({id:post.id});
+
+            $scope.posts = $scope.posts.filter(function (element) {
+                return element.id !== post.id;
+            });
+        }
+    }
+    PostController.$inject = ['$scope','$routeParams','$resource'];
+    function PostController($scope,$routeParams,$resource){
+        var Post = $resource('https://jsonplaceholder.typicode.com/posts/:id', {id: "@id"});
+        $scope.post = Post.get({id:$routeParams.id});
     }
 }());
